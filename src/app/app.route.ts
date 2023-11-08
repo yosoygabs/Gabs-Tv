9e6aa6f1ba5b84f62a3cff4b93eebde4a0fc5802
Gabriel <cuerdaguijarrogabriel@gmail.com>
2023-11-08 18:06:46 +0100

    landingpage y home decentes

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { GamesComponent } from './components/games/games.component';
+import { HomeComponent } from './components/home/home.component';
 import { Component } from '@angular/core';
 
 export const APP_ROUTE: Route[] = [
@@ -9,13 +9,17 @@ export const APP_ROUTE: Route[] = [
         loadComponent: () => import('./components/welcome/welcome.component').then((m) => m.WelcomeComponent) ,
     },
     {
+        path: 'home',
+        loadComponent: () => import('./components/home/home.component').then((m) => m.HomeComponent),
+    },
+    {
         path: 'games',
-        loadComponent: () => import('./components/games/games.component').then((m) => m.GamesComponent) ,
+        loadComponent: () => import('./components/games/games.component').then((m) => m.GamesComponent),
     },
     {
         path: 'about',
-        loadComponent: () => import('./components/about/about.component').then((m) => m.AboutComponent) ,
-    },
+        loadComponent: () => import('./components/about/about.component').then((m) => m.AboutComponent),
+    },    
     { path: '', redirectTo: '/welcome', pathMatch: 'full' },
     { path: '**', redirectTo: '/welcome' },
 ];
--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-
+import { RouterModule, RouterLink } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
 @Component({
-  selector: 'app-welcome',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+    selector: 'app-welcome',
+    standalone: true,
+    templateUrl: './welcome.component.html',
+    styleUrls: ['./welcome.component.scss'],
+    imports: [CommonModule, RouterModule]
 })
 export class WelcomeComponent {
-
+  
+  
 }
